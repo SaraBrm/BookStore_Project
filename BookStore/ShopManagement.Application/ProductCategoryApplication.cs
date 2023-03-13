490c3961e74148461ac6a4856744bf7cf2b90025
sarabrm <sarabayrami01@gmail.com>
2023-03-13 12:08:03 -0700

    Product Category Domain & Mappings & Application & Repository Implemented

---
+++ BookStore/ShopManagement.Application/ProductCategoryApplication.cs
@@ -0,0 +1,64 @@
+﻿using _0_Framework.Application;
+using ShopManagement.Application.Contracts.ProductCategory;
+using ShopManagement.Domain.ProductCategoryAgg;
+using System.Collections.Generic;
+
+namespace ShopManagement.Application
+{
+    public class ProductCategoryApplication : IProductCategoryApplication
+    {
+        private readonly IProductCategoryRepository _productCategoryRepository;
+
+        public ProductCategoryApplication(IProductCategoryRepository productCategoryRepository)
+        {
+            _productCategoryRepository = productCategoryRepository;
+        }
+
+        public OperationResult Create(CreateProductCategory command)
+        {
+            var operation = new OperationResult();
+            
+            if (_productCategoryRepository.Exists(x=>x.Name==command.Name))
+                return operation.Failed("امکان درج رکورد تکراری وجود ندارد. لطفا دوباره تلاش کنید.");
+            
+            var slug = command.Slug.Slugify();
+            var productCategory = new ProductCategory(command.Name, command.Description,
+                command.Picture, command.PictureAlt, command.PictureTitle, command.Keywords,
+                command.MetaDescription, slug);
+           
+            _productCategoryRepository.Create(productCategory);
+            _productCategoryRepository.SaveChanges();
+            return operation.Succedded();
+        }
+
+        public OperationResult Edit(EditProductCategory command)
+        {
+            var operation=new OperationResult();
+            var productCategory = _productCategoryRepository.GetT(command.Id);
+            
+            if (productCategory == null)
+                return operation.Failed("رکورد با اطاعات درخواست شده یافت نشد. لطفا دوباره تلاش کنید.");
+            
+            if (_productCategoryRepository.Exists(x => x.Name == command.Name && x.Id != x.Id))
+                return operation.Failed("رکورد با اطاعات درخواست شده یافت نشد. لطفا دوباره تلاش کنید.");
+            
+            var slug=command.Slug.Slugify();
+            productCategory.Edit(command.Name, command.Description,
+                command.Picture, command.PictureAlt, command.PictureTitle, command.Keywords,
+                command.MetaDescription, slug);
+            
+            _productCategoryRepository.SaveChanges();
+            return operation.Succedded();
+        }
+
+        public EditProductCategory GetDetails(long id)
+        {
+            return _productCategoryRepository.GetDetails(id);
+        }
+
+        public List<ProducrCategoryViewModel> Search(ProductCategorySearchModel searchModel)
+        {
+            return _productCategoryRepository.Search(searchModel);
+        }
+    }
+}
---
+++ BookStore/ShopManagement.Domain/ProductCategoryAgg/ProductCategory.cs
@@ -0,0 +1,45 @@
+﻿using _0_Framework.Domain;
+
+namespace ShopManagement.Domain.ProductCategoryAgg
+{
+    public class ProductCategory: EntityBase
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Picture { get; set; }
+        public string PictureAlt { get; set; }
+        public string PictureTitle { get; set; }
+        public string Keywords { get; set; }
+        public string MetaDescription { get; set; }
+        public string Slug { get; set; }
+
+        public ProductCategory(string name, string description, string picture, 
+            string pictureAlt, string pictureTitle, string keywords,
+            string metaDescription, string slug)
+        {
+            Name = name;
+            Description = description;
+            Picture = picture;
+            PictureAlt = pictureAlt;
+            PictureTitle = pictureTitle;
+            Keywords = keywords;
+            MetaDescription = metaDescription;
+            Slug = slug;
+        }
+
+        public void Edit(string name, string description, string picture,
+            string pictureAlt, string pictureTitle, string keywords,
+            string metaDescription, string slug)
+        {
+            Name = name;
+            Description = description;
+            Picture = picture;
+            PictureAlt = pictureAlt;
+            PictureTitle = pictureTitle;
+            Keywords = keywords;
+            MetaDescription = metaDescription;
+            Slug = slug;
+        }
+
+    }
+}
