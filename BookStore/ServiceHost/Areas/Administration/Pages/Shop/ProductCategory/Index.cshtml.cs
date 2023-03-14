b4ad5fca52e13c8d94acf00901d2efb0b0a9c484
sarabrm <sarabayrami01@gmail.com>
2023-03-14 05:51:20 -0700

    Search & List Product Categories Implemented

---
+++ BookStore/ServiceHost/Areas/Administration/Pages/Shop/ProductCategory/Index.cshtml.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ShopManagement.Application.Contracts.ProductCategory;
+using System.Collections.Generic;
+
+namespace ServiceHost.Areas.Administration.Pages.Shop.ProductCategory
+{
+    public class IndexModel : PageModel
+    {
+        public ProductCategorySearchModel SearchModel;
+        public List<ProducrCategoryViewModel> ProductCategories;
+        
+        private readonly IProductCategoryApplication _productCategoryApplication;
+
+        public IndexModel(IProductCategoryApplication productCategoryApplication)
+        {
+            _productCategoryApplication = productCategoryApplication;
+        }
+
+        public void OnGet(ProductCategorySearchModel searchModel)
+        {
+            ProductCategories = _productCategoryApplication.Search(searchModel);
+        }
+    }
+}
