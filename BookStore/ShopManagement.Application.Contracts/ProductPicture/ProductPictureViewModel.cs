0b0b035a9b640295f41875af8d035c187818c7e9
sarabrm <sarabayrami01@gmail.com>
2023-03-16 09:59:37 -0700

    Product Picture Domain & Application Implemented

---
+++ BookStore/ShopManagement.Application.Contracts/ProductPicture/ProductPictureViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ShopManagement.Application.Contracts.ProductPicture
+{
+    public class ProductPictureViewModel
+    {
+        public long Id { get; set; }
+        public long Product { get; set; }
+        public string Picture { get; set; }
+        public string CreationDate { get; set; }
+
+    }
+}
