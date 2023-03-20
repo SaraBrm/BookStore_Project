9a3281cefd97f395eba386b261f1b973be5d4b14
sarabrm <sarabayrami01@gmail.com>
2023-03-20 12:20:20 -0700

    slider in ui implemented

---
+++ BookStore/01_BookStoreQuery/Query/SlideQuery.cs
@@ -0,0 +1,32 @@
+﻿using _01_BookStoreQuery.Contracts.Slide;
+using ShopManagement.Infrastructure.EFCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01_BookStoreQuery.Query
+{
+    public class SlideQuery : ISlideQuery
+    {
+        private readonly ShopContext _shopContext;
+
+        public SlideQuery(ShopContext shopContext)
+        {
+            _shopContext = shopContext;
+        }
+
+        public List<SlideQueryModel> GetSlides()
+        {
+            return _shopContext.Slides.Where(x => x.IsRemoved == false).Select(x => new SlideQueryModel
+            {
+                Picture = x.Picture,
+                PictureAlt = x.PictureAlt,
+                PictureTitle = x.PictureTitle,
+                Heading = x.Heading,
+                Title = x.Title,
+                Text = x.Text,
+                BtnText = x.BtnText,
+                Link = x.Link
+            }).ToList();
+        }
+    }
+}
