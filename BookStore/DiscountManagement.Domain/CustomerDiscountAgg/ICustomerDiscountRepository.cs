0e13664030e78c16799acb69501113cfa6602010
sarabrm <sarabayrami01@gmail.com>
2023-03-22 11:44:50 -0700

    Customer Discount Domain Implemented - Application & Repository Structure Created

---
+++ BookStore/DiscountManagement.Domain/CustomerDiscountAgg/ICustomerDiscountRepository.cs
@@ -0,0 +1,10 @@
+﻿using _0_Framework.Domain;
+
+namespace DiscountManagement.Domain.CustomerDiscountAgg
+{
+    public interface ICustomerDiscountRepository:IRepository<long, CustomerDiscount>
+    {
+        EditCustomerDiscount GetDetails(long id);
+        List<CustomerDiscountViewModel> Search(CustomerDiscountSearchModel searchModel);
+    }
+}
