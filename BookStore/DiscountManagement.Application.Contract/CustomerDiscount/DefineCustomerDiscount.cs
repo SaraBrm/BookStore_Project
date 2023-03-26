7852975b8bddd10fd1d275cf5869f5a7673456a0
sarabrm <sarabayrami01@gmail.com>
2023-03-26 22:03:16 +0430

    Colleague Discount Management Implemented

--- BookStore/DiscountManagement.Application.Contract/CustomerDiscount/DefineCustomerDiscount.cs
+++ BookStore/DiscountManagement.Application.Contract/CustomerDiscount/DefineCustomerDiscount.cs
@@ -7,13 +7,16 @@ namespace DiscountManagement.Application.Contract.CustomerDiscount
 {
     public class DefineCustomerDiscount
     {
+        [Range(1, 100000, ErrorMessage = ValidationMessages.IsRequired)]
         public long ProductId { get; set; }
 
-        [Required(ErrorMessage = ValidationMessages.IsRequired)]
+        [Range(1, 99, ErrorMessage = ValidationMessages.IsRequired)]
         public int DiscountRate { get; set; }
 
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string StartDate { get; set; }
 
+        [Required(ErrorMessage =ValidationMessages.IsRequired)]
         public string EndDate { get; set; }
 
         [Required(ErrorMessage = ValidationMessages.IsRequired)]
---
+++ BookStore/DiscountManagement.Infrastructure.EFCore/Repository/ColleagueDiscountRepository.cs
@@ -0,0 +1,53 @@
+﻿using _0_Framework.Application;
+using _0_Framework.Infrastucture;
+using DiscountManagement.Application.Contract.ColleagueDiscount;
+using DiscountManagement.Domain.ColleagueDiscountAgg;
+using ShopManagement.Infrastructure.EFCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DiscountManagement.Infrastructure.EFCore.Repository
+{
+    public class ColleagueDiscountRepository : RepositoryBase<long, ColleagueDiscount>, IColleagueDiscountRepository
+    {
+        private readonly DiscountContext _discountContext;
+        private readonly ShopContext _shopContext;
+
+        public ColleagueDiscountRepository(DiscountContext discountContext, ShopContext shopContext):base(discountContext)
+        {
+            _discountContext = discountContext;
+            _shopContext = shopContext;
+        }
+
+        public EditColleagueDiscount GetDetails(long id)
+        {
+            return _discountContext.CustomerDiscounts.Select(x=>new EditColleagueDiscount
+            {
+                Id=x.Id,
+                DiscountRate=x.DiscountRate,
+                ProductId=x.ProductId
+            }).FirstOrDefault(x=>x.Id==id);
+        }
+
+        public List<ColleagueDiscountViewModel> Search(ColleagueDiscountSearchModel searchModel)
+        {
+            var products = _shopContext.Products.Select(x => new { x.Id, x.Name }).ToList();
+            var query = _discountContext.ColleagueDiscounts.Select(x => new ColleagueDiscountViewModel
+            {
+                Id = x.Id,
+                CreationDate = x.CreationDate.ToFarsi(),
+                DiscountRate = x.DiscountRate,
+                ProductId = x.ProductId,
+                IsRemoved=x.IsRemoved
+            });
+
+            if (searchModel.ProductId > 0)
+                query = query.Where(x => x.ProductId == searchModel.ProductId);
+
+            var discounts = query.OrderByDescending(x => x.Id).ToList();
+            discounts.ForEach(discount => discount.Product = 
+                products.FirstOrDefault(x => x.Id == discount.ProductId)?.Name);
+            return discounts;
+        }
+    }
+}
