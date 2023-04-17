980f2fe31d6830c26fc8e753b6fe74ceb9a8ad3d
sarabrm <sarabayrami01@gmail.com>
2023-04-17 20:30:20 +0430

    Article Category Mapping & Repository Implemented.

---
+++ BookStore/BlogManagement.Infrastructure.EFCore/Repository/ArticleCategoryRepository.cs
@@ -0,0 +1,51 @@
+﻿using _0_Framework.Infrastucture;
+using BlogManagement.Application.Contracts.ArticleCategory;
+using BlogManagement.Domain.ArticleCategoryAgg;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BlogManagement.Infrastructure.EFCore.Repository
+{
+    public class ArticleCategoryRepository : RepositoryBase<long, ArticleCategory>, IArticleCategoryRepository
+    {
+        private readonly BlogContext _blogContext;
+        private object acvm;
+
+        public ArticleCategoryRepository(BlogContext blogContext):base(blogContext)
+        {
+            _blogContext = blogContext;
+        }
+
+        public EditArticleCategory GetDetails(long id)
+        {
+            return _blogContext.ArticleCategories.Select(x => new EditArticleCategory
+            {
+                Id = x.Id,
+                Name = x.Name,
+                CanonicalAddress = x.CanonicalAddress,
+                Description = x.Description,
+                Keywords = x.Keywords,
+                MetaDescription = x.MetaDescription,
+                ShowOrder = x.ShowOrder,
+                Slug = x.Slug
+            }).FirstOrDefault(x => x.Id == id);
+        }
+
+        public List<ArticleCategoryViewModel> Search(ArticleCategorySearchModel searchModel)
+        {
+            var query = _blogContext.ArticleCategories.Select(x => new ArticleCategoryViewModel
+            {
+                Id = x.Id,
+                Name = x.Name,
+                Description = x.Description,
+                Picture = x.Picture,
+                ShowOrder = x.ShowOrder
+            });
+
+            if (!string.IsNullOrWhiteSpace(searchModel.Name))
+                query = query.Where(x => x.Name.Contains(searchModel.Name));
+
+            return query.OrderByDescending(x => x.ShowOrder).ToList();
+        }
+    }
+}
