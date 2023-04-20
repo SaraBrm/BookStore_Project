f3caced9e0565565ef527a40a5abfdb0a0425116
sarabrm <sarabayrami01@gmail.com>
2023-04-20 17:36:44 +0430

    Article UI (Index & Create) Implemented.

--- BookStore/0_Framework/Application/ValidationMessages.cs
+++ BookStore/0_Framework/Application/ValidationMessages.cs
@@ -5,5 +5,6 @@
         public const string IsRequired = "پر کردن این فیلد اجباری است.";
         public const string MaxFileSize = "فایل بزرگتر از حد مجاز است.";
         public const string InvalidFileFormat = "فرمت فایل های مجاز jpeg | jpg | png می باشد.";
+        public const string MaxLenght = "مقدار وارد شده بیش از طول مجاز است";
     }
 }
--- BookStore/BlogManagement.Domain/ArticleCategoryAgg/IArticleCategoryRepository.cs
+++ BookStore/BlogManagement.Domain/ArticleCategoryAgg/IArticleCategoryRepository.cs
@@ -9,5 +9,6 @@ namespace BlogManagement.Domain.ArticleCategoryAgg
         List<ArticleCategoryViewModel> Search(ArticleCategorySearchModel searchModel);
         EditArticleCategory GetDetails(long id);
         string GetSlugBy(long id);
+        List<ArticleCategoryViewModel> GetArticleCategories();
     }
 }
