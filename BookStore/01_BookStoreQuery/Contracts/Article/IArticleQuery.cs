351097edd8d80734de33c95942b3d300092511c3
sarabrm <sarabayrami01@gmail.com>
2023-04-21 15:43:17 +0430

    Latest Article View Component

---
+++ BookStore/01_BookStoreQuery/Contracts/Article/IArticleQuery.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace _01_BookStoreQuery.Contracts.Article
+{
+    public interface IArticleQuery
+    {
+        List<ArticleQueryModel> LatestArticles();
+    }
+}
---
+++ BookStore/01_BookStoreQuery/Query/ArticleQuery.cs
@@ -0,0 +1,41 @@
+﻿using _0_Framework.Application;
+using _01_BookStoreQuery.Contracts.Article;
+using BlogManagement.Infrastructure.EFCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01_BookStoreQuery.Query
+{
+    public class ArticleQuery : IArticleQuery
+    {
+        private readonly BlogContext _blogContext;
+
+        public ArticleQuery(BlogContext blogContext)
+        {
+            _blogContext = blogContext;
+        }
+
+        public List<ArticleQueryModel> LatestArticles()
+        {
+            return _blogContext.Articles.Include(x => x.Category).Where(x => x.PublishDate <= DateTime.Now).Select(x => new ArticleQueryModel
+            {
+                Title = x.Title,
+                ShortDescription = x.ShortDescription,
+                Description = x.Description,
+                Picture = x.Picture,
+                PictureAlt = x.PictureAlt,
+                PictureTitle = x.PictureTitle,
+                PublishDate = x.PublishDate.ToFarsi(),
+                Slug = x.Slug,
+                MetaDescription = x.MetaDescription,
+                Keywords = x.Keywords,
+                CanonicalAddress = x.CanonicalAddress,
+                CategoryId = x.CategoryId,
+                CategoryName = x.Category.Name,
+                CategorySlug = x.Category.Slug
+            }).ToList();
+        }
+    }
+}
